6043a525ce946becbebf5dbb84c1c184bb4d9f22
Shahi77 <shahiinfinity02@gmail.com>
2024-01-16 18:26:57 +0530

    Nested divs

--- App.js
+++ App.js
@@ -1,3 +1,25 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+/* Nested structures
+<div id = "parent">
+<div id="child">
+<h1>Hiii!!</h1>
+</div>
+</div>
+ */
+const parent = React.createElement(
+  //Prent div
+  "div",
+  { id: "parent" },
+  React.createElement(
+    //Child div
+    "div",
+    { id: "child" },
+    React.createElement(
+      //h1 tag
+      "h1",
+      {},
+      "Hiii!!"
+    )
+  )
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
