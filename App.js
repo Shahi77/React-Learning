9b114844398d31561ed121da3aeb97c9f85e8639
Shahi77 <shahiinfinity02@gmail.com>
2024-01-18 18:51:47 +0530

    intro to jsx and react components

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
-/* Nested structures
-<div id = "parent">
-<div id="child">
-<h1>Hiii!!</h1> 
-<h2>Hello!!</h2>
-</div>
-</div>
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement(
-  //Prent div
-  "div",
-  { id: "parent" },
-  React.createElement(
-    //Child div
-    "div",
-    { id: "child" },
-    [
-      //Create array for siblings
-      React.createElement(
-        //h1 tag
-        "h1",
-        {},
-        "Hiii!!"
-      ),
-      React.createElement(
-        //h2 tag
-        "h2",
-        {},
-        "Hello!!"
-      ),
-    ]
-  )
+
+//React Element
+const heading = <h1 id="heading">Helo...</h1>;
+
+//React Functional component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h1 className="heading">React Functional Component</h1>
+    </div>
+  );
+};
+
+//Component composition
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hiii!!
+  </h1>
+);
+const HeadingComponent2 = () => (
+  <div id="container">
+    <Title />
+    <h2 className="heading">React</h2>
+  </div>
+);
+
+//Javascript inside of jsx
+const number = 1000;
+const HeadingComponent3 = () => (
+  <div id="container">
+    {/* react element inside fucntion component -> {heading} */}
+    {number}
+    <h3>{200 + 300}</h3>
+    <h2 className="heading">React</h2>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent3 />);
