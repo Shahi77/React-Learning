531d1806cdabaadac04f2b981f03d32c6638d74b
Shahi77 <shahiinfinity02@gmail.com>
2024-01-21 13:59:35 +0530

    introduction to hooks

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-/_
-Foody app:-
-Header
--Logo
--Nav Items
-Body
--Search
--RestaurantContainer
--RestaurantCard
--Img
--Name of Restaurant
--Star Rating
--cuisine
--delivery time
-Footer
--Copyright
--Links
--Address
--Contact
-_/
+# Foody app:-
+
+- - Header
+- -Logo
+- -Nav Items
+- Body
+- -Search
+- -RestaurantContainer
+- -RestaurantCard
+- -Img
+- -Name of Restaurant
+- -Star Rating
+- -cuisine
+- -delivery time
+- Footer
+- -Copyright
+- -Links
+- -Address
+- -Contact
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="rest-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
