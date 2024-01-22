bb731f5c00d252093f5d994cf74b69b60ad5f275
Shahi77 <shahiinfinity02@gmail.com>
2024-01-22 09:56:33 +0530

    fetching data from api

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.001213&lng=75.9608385&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json);
+    const arrayOfCards = json.data.cards;
+    const restaurant_list = "restaurant_grid_listing";
+
+    for (const cardObj of arrayOfCards) {
+      if (cardObj.card.card && cardObj.card.card.id === restaurant_list) {
+        const resData =
+          cardObj.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        console.log(resData);
+        setListOfRestaurants(resData);
+      }
+    }
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <h1>Loading</h1>;
+  }
   return (
     <div className="body">
       <div className="filter">
@@ -21,7 +47,7 @@ const Body = () => {
       </div>
       <div className="rest-container">
         {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          <RestaurantCard key={restaurant?.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
