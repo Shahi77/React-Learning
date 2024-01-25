33298d49e1134d26b9a8ccceeadbec2ea989714f
Shahi77 <shahiinfinity02@gmail.com>
2024-01-25 21:24:54 +0530

    intro to class based components

---
+++ src/Components/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h1>Name: Shahi</h1>
+      <h2>Place: Hamirpur</h2>
+    </div>
+  );
+};
+export default User;
---
+++ src/Components/UserClass.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h1>Hii</h1>
+      </div>
+    );
+  }
+}
+export default UserClass;
